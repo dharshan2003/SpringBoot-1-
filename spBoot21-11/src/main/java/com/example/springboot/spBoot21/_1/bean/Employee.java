40ea73fdf3d944f6f8ec9caa4d2a0521dfb649d6
dharshan2003 <112673415+dharshan2003@users.noreply.github.com>
2025-12-03 11:24:20 +0530

    Add files via upload

---
+++ spBoot21-11/src/main/java/com/example/springboot/spBoot21/_1/bean/Employee.java
@@ -0,0 +1,18 @@
+package com.example.springboot.spBoot21._1.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+	
+	
+	int id;
+	String name;
+	
+	
+}
